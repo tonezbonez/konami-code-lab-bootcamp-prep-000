3e20ed0092242445b9e8e9c45c1c226d1dfdc1e2
Anthony Bracewell <anthonybracewell.violinist@gmail.com>
2017-11-27 20:27:30 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -20,7 +20,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 */
 function init(){
   let index = 0;
-  document.body.addEventListener('keydown', function(event){ 
+  document.body.addEventListener('keydown', function(event){
     const key = event.which
     if (key===code[index]){
       index++;
