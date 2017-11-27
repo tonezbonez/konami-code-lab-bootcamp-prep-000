a14566a6fe71ac06a8be17910d7261a4c2751fdf
Anthony Bracewell <anthonybracewell.violinist@gmail.com>
2017-11-27 20:12:55 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -15,5 +15,5 @@ function init() {
         index = 0;
       }
     }
-  document.body.addEventListener('keydown', onKeyDownHandler(i.location))
+  document.body.addEventListener('keydown', onKeyDownHandler(i.which))
 }
